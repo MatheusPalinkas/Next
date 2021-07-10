fa9161a5df226964b63115885e30a5e930ce3d55
Palinkas <matheuspalinkas31@gmail.com>
2021-07-10 15:35:12 -0300

    Site de teste

---
+++ pages/test/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from 'next/link'
+
+// import { Container } from './styles';
+
+const test: React.FC = () => {
+  return <h1>Olá mundo <Link href="/">Voltar pra home</Link></h1>;
+}
+
+export default test;
